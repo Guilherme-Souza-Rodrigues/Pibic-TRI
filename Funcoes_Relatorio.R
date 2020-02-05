ee277a94989264feefc55b580153e490caf0516c
Vitor Macedo <vmacedo99@gmail.com>
2020-02-05 01:45:14 -0300

    Update Funcoes_Relatorio.R

--- Funcoes_Relatorio.R
+++ Funcoes_Relatorio.R
@@ -85,356 +85,209 @@ dico <- function(){for(prova in 1:n.provas) {
 # Cálculo dos estimadores dos parâmetros dos itens e do estimador do parâmetro de habilidade de
 #cada aluno via Monte Carlo Cadeia de Markov:
 mcmc<- function() {
-  
   model <- stan_model(model_code = 
                         
                         "data {
-                      
                       int<lower=1> N_alunos;              // number of students
-                      
                       int<lower=1> N_itens;              // number of questions
-                      
                       int<lower=1> N;                   // number of observations
-                      
                       int<lower=1,upper=N_alunos> aluno[N];   // student for observation n
-                      
                       int<lower=1,upper=N_itens> item[N];   // question for observation n
-                      
                       int<lower=0,upper=1> y[N];     // correctness for observation n
                       
 }
 
+transformed data{
+int mu;
+int sigma;
+mu=0;
+sigma=1;
+
+}
+
 parameters{
-
 vector[N_itens] b;
-
 vector<lower=0,upper=1>[N_itens] c;
-
 vector[N_alunos] theta;
-
 vector<lower=0>[N_itens] a;
 
 }
 
 model{
-
 a~normal(0,1);
-
 b~normal(0,1);
-
 c~beta(5,17);
-
 theta~normal(0,1);
-
-y~bernoulli(c[item]+((1-c[item]).*inv_logit((a[item] .*theta[aluno])-b[item])));
+y~bernoulli(c[item]+((1-c[item]) *normal_cdf((a[item] .*theta[aluno])-b[item],mu,sigma)));
 
 }"
 
   )
   
-  
-  
-  banco_respostas <- dados.original%>%
-    
-    dplyr::select(Matricula,Nome.questao,Acertou,Numero.prova)%>%
-    
-    mutate(Nome.questao=as.character(Nome.questao),
-           
-           Matricula=as.numeric(Matricula))
-  
-  
-  
-  ##############
-  
-  #  P1
-  
-  ##############
-  
-  
-  
-  dados_p1 <- banco_respostas%>%
-    
-    filter(Numero.prova==1)%>%
-    
-    na.omit(Acertou)%>%
-    
-    mutate(Matricula.mod=as.numeric(as.factor(Matricula)),
-           
-           Nome.questao.mod=as.numeric(as.factor(Nome.questao)))%>%
-    
-    arrange(Matricula.mod,Nome.questao.mod)
-  
-  
-  
-  Matriculas_equivalencia_p1 <- dados_p1%>%
-    
-    dplyr::select(Matricula,Matricula.mod)%>%
-    
-    distinct()%>%
-    
-    arrange(Matricula.mod)
-  
-  
-  
-  Questoes_equivalencia_p1 <- dados_p1%>%
-    
-    dplyr::select(Nome.questao,Nome.questao.mod)%>%
-    
-    distinct()%>%
-    
-    arrange(Nome.questao.mod)
-  
-  fit.p1 <- sampling(object=model,data=list(N_alunos=length(unique(dados_p1$Matricula.mod)),
-                                            
-                                            N_itens=length(unique(dados_p1$Nome.questao.mod)),
-                                            
-                                            N=nrow(dados_p1),
-                                            
-                                            aluno=dados_p1$Matricula.mod,
-                                            
-                                            item=dados_p1$Nome.questao.mod, 
-                                            
-                                            y=dados_p1$Acertou),iter=niter,chains=nchains,cores=4)
-  
-  params.p1 <- extract(fit.p1)
-  
-  itens.p1 <- array(c(params.p1$a,params.p1$b,params.p1$c),
-                    
+banco_respostas <- dados.original%>%
+  dplyr::select(Matricula,Nome.questao,Acertou,Numero.prova)%>%
+  mutate(Nome.questao=as.character(Nome.questao),
+         Matricula=as.numeric(Matricula))
+##############
+#  P1
+##############
+dados_p1 <- banco_respostas%>%
+  filter(Numero.prova==1)%>%
+  na.omit(Acertou)%>%
+  mutate(Matricula.mod=as.numeric(as.factor(Matricula)),
+         Nome.questao.mod=as.numeric(as.factor(Nome.questao)))%>%
+  arrange(Matricula.mod,Nome.questao.mod)
+
+Matriculas_equivalencia_p1 <- dados_p1%>%
+  dplyr::select(Matricula,Matricula.mod)%>%
+  distinct()%>%
+  arrange(Matricula.mod)
+
+Questoes_equivalencia_p1 <- dados_p1%>%
+  dplyr::select(Nome.questao,Nome.questao.mod)%>%
+  distinct()%>%
+  arrange(Nome.questao.mod)
+
+fit.p1 <- sampling(object=model,data=list(N_alunos=length(unique(dados_p1$Matricula.mod)),
+                                          N_itens=length(unique(dados_p1$Nome.questao.mod)),
+                                          N=nrow(dados_p1),
+                                          aluno=dados_p1$Matricula.mod,
+                                          item=dados_p1$Nome.questao.mod, 
+                                          y=dados_p1$Acertou),iter=niter,chains=nchains,cores=4)
+
+params.p1 <- extract(fit.p1)
+itens.p1 <- array(c(params.p1$a,params.p1$b,params.p1$c),
                     dim = c(nchains*(niter/2) , nrow(Questoes_equivalencia_p1), 3),
-                    
                     dimnames = list(paste("simulação",1:(nchains*(niter/2))),
-                                    
                                     Questoes_equivalencia_p1$Nome.questao,
-                                    
                                     c('a','b','c')))
+
+thetas.mcmc.p1 <- params.p1$theta
+colnames(thetas.mcmc.p1) <- Matriculas_equivalencia_p1$Matricula
+rm(Questoes_equivalencia_p1,Matriculas_equivalencia_p1,dados_p1,fit.p1)
+##############
+#  P2
+##############
+dados_p2 <- banco_respostas%>%
+  filter(Numero.prova==2)%>%
+  na.omit(Acertou)%>%
+  mutate(Matricula.mod=as.numeric(as.factor(Matricula)),
+         Nome.questao.mod=as.numeric(as.factor(Nome.questao)))%>%
+  arrange(Matricula.mod,Nome.questao.mod)
+
+Matriculas_equivalencia_p2 <- dados_p2%>%
+  dplyr::select(Matricula,Matricula.mod)%>%
+  distinct()%>%
+  arrange(Matricula.mod)
   
-  thetas.mcmc.p1 <- params.p1$theta
+Questoes_equivalencia_p2 <- dados_p2%>%
+  dplyr::select(Nome.questao,Nome.questao.mod)%>%
+  distinct()%>%
+  arrange(Nome.questao.mod)
   
-  colnames(thetas.mcmc.p1) <- Matriculas_equivalencia_p1$Matricula
+fit.p2 <- sampling(object=model,data=list(N_alunos=length(unique(dados_p2$Matricula.mod)),
+                                          N_itens=length(unique(dados_p2$Nome.questao.mod)),
+                                          N=nrow(dados_p2),
+                                          aluno=dados_p2$Matricula.mod,
+                                          item=dados_p2$Nome.questao.mod, 
+                                          y=dados_p2$Acertou),iter=niter,chains=nchains,cores=4)
   
-  rm(Questoes_equivalencia_p1,Matriculas_equivalencia_p1,dados_p1,fit.p1)
-  
-  ##############
-  
-  #  P2
-  
-  ##############
-  
-  dados_p2 <- banco_respostas%>%
-    
-    filter(Numero.prova==2)%>%
-    
-    na.omit(Acertou)%>%
-    
-    mutate(Matricula.mod=as.numeric(as.factor(Matricula)),
-           
-           Nome.questao.mod=as.numeric(as.factor(Nome.questao)))%>%
-    
-    arrange(Matricula.mod,Nome.questao.mod)
-  
-  
-  
-  Matriculas_equivalencia_p2 <- dados_p2%>%
-    
-    dplyr::select(Matricula,Matricula.mod)%>%
-    
-    distinct()%>%
-    
-    arrange(Matricula.mod)
-  
-  
-  
-  Questoes_equivalencia_p2 <- dados_p2%>%
-    
-    dplyr::select(Nome.questao,Nome.questao.mod)%>%
-    
-    distinct()%>%
-    
-    arrange(Nome.questao.mod)
-  
-  fit.p2 <- sampling(object=model,data=list(N_alunos=length(unique(dados_p2$Matricula.mod)),
-                                            
-                                            N_itens=length(unique(dados_p2$Nome.questao.mod)),
-                                            
-                                            N=nrow(dados_p2),
-                                            
-                                            aluno=dados_p2$Matricula.mod,
-                                            
-                                            item=dados_p2$Nome.questao.mod, 
-                                            
-                                            y=dados_p2$Acertou),iter=niter,chains=nchains,cores=4)
-  
-  params.p2 <- extract(fit.p2)
-  
-  itens.p2 <- array(c(params.p2$a,params.p2$b,params.p2$c),
-                    
+params.p2 <- extract(fit.p2)
+
+itens.p2 <- array(c(params.p2$a,params.p2$b,params.p2$c),
                     dim = c(nchains*(niter/2) , nrow(Questoes_equivalencia_p2), 3),
-                    
                     dimnames = list(paste("simulação",1:(nchains*(niter/2))),
-                                    
                                     Questoes_equivalencia_p2$Nome.questao,
-                                    
                                     c('a','b','c')))
   
-  thetas.mcmc.p2 <- params.p2$theta
+thetas.mcmc.p2 <- params.p2$theta
+colnames(thetas.mcmc.p2) <- Matriculas_equivalencia_p2$Matricula
+rm(Questoes_equivalencia_p2,Matriculas_equivalencia_p2,dados_p2,fit.p2)
+##############
+#  P3
+##############
+dados_p3 <- banco_respostas%>%
+  filter(Numero.prova==3)%>%
+  na.omit(Acertou)%>%
+  mutate(Matricula.mod=as.numeric(as.factor(Matricula)),
+         Nome.questao.mod=as.numeric(as.factor(Nome.questao)))%>%
+  arrange(Matricula.mod,Nome.questao.mod)
   
-  colnames(thetas.mcmc.p2) <- Matriculas_equivalencia_p2$Matricula
+Matriculas_equivalencia_p3 <- dados_p3%>%
+  dplyr::select(Matricula,Matricula.mod)%>%
+  distinct()%>%
+  arrange(Matricula.mod)
   
-  rm(Questoes_equivalencia_p2,Matriculas_equivalencia_p2,dados_p2,fit.p2)
+Questoes_equivalencia_p3 <- dados_p3%>%
+  dplyr::select(Nome.questao,Nome.questao.mod)%>%
+  distinct()%>%
+  arrange(Nome.questao.mod)
   
-  ##############
+fit.p3 <- sampling(object=model,data=list(N_alunos=length(unique(dados_p3$Matricula.mod)),
+                                          N_itens=length(unique(dados_p3$Nome.questao.mod)),
+                                          N=nrow(dados_p3),
+                                          aluno=dados_p3$Matricula.mod,
+                                          item=dados_p3$Nome.questao.mod, 
+                                          y=dados_p3$Acertou),iter=niter,chains=nchains,cores=4)
   
-  #  P3
-  
-  ##############
-  
-  dados_p3 <- banco_respostas%>%
-    
-    filter(Numero.prova==3)%>%
-    
-    na.omit(Acertou)%>%
-    
-    mutate(Matricula.mod=as.numeric(as.factor(Matricula)),
-           
-           Nome.questao.mod=as.numeric(as.factor(Nome.questao)))%>%
-    
-    arrange(Matricula.mod,Nome.questao.mod)
-  
-  
-  
-  Matriculas_equivalencia_p3 <- dados_p3%>%
-    
-    dplyr::select(Matricula,Matricula.mod)%>%
-    
-    distinct()%>%
-    
-    arrange(Matricula.mod)
-  
-  
-  
-  Questoes_equivalencia_p3 <- dados_p3%>%
-    
-    dplyr::select(Nome.questao,Nome.questao.mod)%>%
-    
-    distinct()%>%
-    
-    arrange(Nome.questao.mod)
-  
-  fit.p3 <- sampling(object=model,data=list(N_alunos=length(unique(dados_p3$Matricula.mod)),
-                                            
-                                            N_itens=length(unique(dados_p3$Nome.questao.mod)),
-                                            
-                                            N=nrow(dados_p3),
-                                            
-                                            aluno=dados_p3$Matricula.mod,
-                                            
-                                            item=dados_p3$Nome.questao.mod, 
-                                            
-                                            y=dados_p3$Acertou),iter=niter,chains=nchains,cores=4)
-  
-  params.p3 <- extract(fit.p3)
-  
-  itens.p3 <- array(c(params.p3$a,params.p3$b,params.p3$c),
-                    
+params.p3 <- extract(fit.p3)
+
+itens.p3 <- array(c(params.p3$a,params.p3$b,params.p3$c),
                     dim = c(nchains*(niter/2) , nrow(Questoes_equivalencia_p3), 3),
-                    
                     dimnames = list(paste("simulação",1:(nchains*(niter/2))),
-                                    
                                     Questoes_equivalencia_p3$Nome.questao,
-                                    
                                     c('a','b','c')))
   
-  thetas.mcmc.p3 <- params.p3$theta
+thetas.mcmc.p3 <- params.p3$theta
+colnames(thetas.mcmc.p3) <- Matriculas_equivalencia_p3$Matricula
+rm(Questoes_equivalencia_p3,Matriculas_equivalencia_p3,dados_p3,fit.p3)
   
-  colnames(thetas.mcmc.p3) <- Matriculas_equivalencia_p3$Matricula
+##############
+#  P4-subs
+##############
+dados_p4 <- banco_respostas%>%
+  filter(Numero.prova==4)%>%
+  na.omit(Acertou)%>%
+  mutate(Matricula.mod=as.numeric(as.factor(Matricula)),
+         Nome.questao.mod=as.numeric(as.factor(Nome.questao)))%>%
+  arrange(Matricula.mod,Nome.questao.mod)
   
-  rm(Questoes_equivalencia_p3,Matriculas_equivalencia_p3,dados_p3,fit.p3)
+Matriculas_equivalencia_p4 <- dados_p4%>%
+  dplyr::select(Matricula,Matricula.mod)%>%
+  distinct()%>%
+  arrange(Matricula.mod)
   
-  ##############
+Questoes_equivalencia_p4 <- dados_p4%>%
+  dplyr::select(Nome.questao,Nome.questao.mod)%>%
+  distinct()%>%
+  arrange(Nome.questao.mod)
   
-  #  P4-subs
-  
-  ##############
-  
-  dados_p4 <- banco_respostas%>%
-    
-    filter(Numero.prova==4)%>%
-    
-    na.omit(Acertou)%>%
-    
-    mutate(Matricula.mod=as.numeric(as.factor(Matricula)),
-           
-           Nome.questao.mod=as.numeric(as.factor(Nome.questao)))%>%
-    
-    arrange(Matricula.mod,Nome.questao.mod)
-  
-  
-  
-  Matriculas_equivalencia_p4 <- dados_p4%>%
-    
-    dplyr::select(Matricula,Matricula.mod)%>%
-    
-    distinct()%>%
-    
-    arrange(Matricula.mod)
-  
-  
-  
-  Questoes_equivalencia_p4 <- dados_p4%>%
-    
-    dplyr::select(Nome.questao,Nome.questao.mod)%>%
-    
-    distinct()%>%
-    
-    arrange(Nome.questao.mod)
-  
-  fit.p4 <- sampling(object=model,data=list(N_alunos=length(unique(dados_p4$Matricula.mod)),
-                                            
-                                            N_itens=length(unique(dados_p4$Nome.questao.mod)),
-                                            
-                                            N=nrow(dados_p4),
-                                            
-                                            aluno=dados_p4$Matricula.mod,
-                                            
-                                            item=dados_p4$Nome.questao.mod, 
-                                            
-                                            y=dados_p4$Acertou),iter=niter,chains=nchains,cores=4)
-  
-  params.p4 <- extract(fit.p4)
-  
-  itens.p4 <- array(c(params.p4$a,params.p4$b,params.p4$c),
-                    
+fit.p4 <- sampling(object=model,data=list(N_alunos=length(unique(dados_p4$Matricula.mod)),
+                                          N_itens=length(unique(dados_p4$Nome.questao.mod)),
+                                          N=nrow(dados_p4),
+                                          aluno=dados_p4$Matricula.mod,
+                                          item=dados_p4$Nome.questao.mod, 
+                                          y=dados_p4$Acertou),iter=niter,chains=nchains,cores=4)
+
+params.p4 <- extract(fit.p4)
+
+itens.p4 <- array(c(params.p4$a,params.p4$b,params.p4$c),
                     dim = c(nchains*(niter/2) , nrow(Questoes_equivalencia_p4), 3),
-                    
                     dimnames = list(paste("simulação",1:(nchains*(niter/2))),
-                                    
                                     Questoes_equivalencia_p4$Nome.questao,
-                                    
                                     c('a','b','c')))
   
-  
-  
-  thetas.mcmc.p4 <- params.p4$theta
-  
-  colnames(thetas.mcmc.p4) <- Matriculas_equivalencia_p4$Matricula
-  
-  rm(Questoes_equivalencia_p4,Matriculas_equivalencia_p4,dados_p4,fit.p4)
-  
-  #########################
-  
-  #  Juntando tudo
-  
-  #########################
-  
-  mcmc.itens <- list(itens.p1,itens.p2,itens.p3,itens.p4)
-  
-  mcmc.theta <- list(thetas.mcmc.p1,thetas.mcmc.p2,thetas.mcmc.p3,thetas.mcmc.p4)
-  
-  save(mcmc.itens, file = "Parametros_Itens.RData")
-  
-  return(list(thetas=mcmc.theta,itens=mcmc.itens))
-  
-  }
+thetas.mcmc.p4 <- params.p4$theta
+colnames(thetas.mcmc.p4) <- Matriculas_equivalencia_p4$Matricula
+rm(Questoes_equivalencia_p4,Matriculas_equivalencia_p4,dados_p4,fit.p4)
+#########################
+#  Juntando tudo
+#########################
+mcmc.itens <- list(itens.p1,itens.p2,itens.p3,itens.p4)
+mcmc.theta <- list(thetas.mcmc.p1,thetas.mcmc.p2,thetas.mcmc.p3,thetas.mcmc.p4)
+save(mcmc.itens, file = "Parametros_Itens.RData")
+return(list(thetas=mcmc.theta,itens=mcmc.itens))
+}
 
 # Dataframe dos coeficientes de cada questao
 data.i <- function(){for (prova in 1:n.provas){
